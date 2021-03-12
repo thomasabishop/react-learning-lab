96d9e498cb148c4838f6f901467d3732fdaee5c1
thomasabishop <tactonbishop@gmail.com>
2021-03-12 12:53:06 +0000

    Central align header, add sticky on scroll

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,58 +1,77 @@
+import React from 'react';
 import {
   makeStyles,
+  useScrollTrigger,
   AppBar,
   Toolbar,
   Typography,
   IconButton,
-} from "@material-ui/core";
-import Logo from "../../src/react-lab-logo.svg";
-import { Link } from "react-router-dom";
-import GitHubIcon from "@material-ui/icons/GitHub";
+} from '@material-ui/core';
+import Logo from '../../src/react-lab-logo.svg';
+import { Link } from 'react-router-dom';
+import GitHubIcon from '@material-ui/icons/GitHub';
+
+// Add sticky on scroll functionality
+function ElevationScroll(props) {
+  const { children } = props;
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
 
 const useStyles = makeStyles(() => ({
-  root: {
-    flexGrow: 1,
+  root: {},
+  header: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
   },
-
   title: {
-    flexGrow: 1,
+    fontSize: '1rem',
+    fontWeight: 500,
+    letterSpacing: '0.6px',
   },
-
   logo: {
-    width: "70px",
+    width: '45px',
   },
 }));
 
-const Header = () => {
+const Header = (props) => {
   const classes = useStyles();
-
   return (
     <div className={classes.root}>
-      <AppBar position="sticky">
-        <Toolbar>
-          <IconButton
-            component={Link}
-            to="/"
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <img className={classes.logo} src={Logo} alt="" />
-          </IconButton>
-          <Typography variant="h5" className={classes.title}>
-            Thomas's React Learning Lab
-          </Typography>
-          <IconButton
-            href="https://github.com/thomasabishop"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-          >
-            <GitHubIcon fontSize="large" />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
+      <ElevationScroll {...props}>
+        <AppBar>
+          <Toolbar variant="dense" className={classes.header}>
+            <IconButton
+              component={Link}
+              to="/"
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+            >
+              <img className={classes.logo} src={Logo} alt="" />
+            </IconButton>
+            <Typography className={classes.title}>
+              Thomas's React Learning Lab
+            </Typography>
+            <IconButton
+              href="https://github.com/thomasabishop"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+            >
+              <GitHubIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <Toolbar />
     </div>
   );
 };
